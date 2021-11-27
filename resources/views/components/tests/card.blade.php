2e56bc1f431c6f52b207509ed5a708df36d3353f
teionhozonn <teionhozonn@gmail.com>
2021-11-28 00:01:59 +0900

    attributes

--- resources/views/components/tests/card.blade.php
+++ resources/views/components/tests/card.blade.php
@@ -1,4 +1,12 @@
-<div class="border-2 shadow-mb w-1/4 p-2">
+@props([
+    'title' => '初期タイトル',
+    'content' => '本文初期値',
+    'message' => 'message',
+])
+
+<div {{ $attributes->merge([
+    'class' => 'border-2 shadow-mb w-1/4 p-2'
+]) }} >
     <div>{{ $title }}</div>
     <div>画像</div>
     <div>{{ $content }}</div>
