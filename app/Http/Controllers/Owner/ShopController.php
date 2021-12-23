56d592576e727988cf83a9167fac885a03e84e8c
teionhozonn <teionhozonn@gmail.com>
2021-12-23 22:47:19 +0900

    shop index

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -34,8 +34,7 @@ class ShopController extends Controller
      */
     public function index()
     {
-        $ownerId = Auth::id();
-        $shops = Shop::where('owner_id', $ownerId)->get();
+        $shops = Shop::where('owner_id', Auth::id())->get();
 
         return view(
             'owner.shops.index',
